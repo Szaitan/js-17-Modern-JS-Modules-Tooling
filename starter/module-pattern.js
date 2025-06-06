20871534ecfa6b9c4a469adbedb3a0d21d0db974
Michal <m.krepiniewicz@gmail.com>
2025-06-06 20:03:38 +0200

    Module patern-rev.01

--- starter/module-pattern.js
+++ starter/module-pattern.js
@@ -1,1 +1,23 @@
 // Module pattern
+// One of the ebst ways of module pattern is to implement effi function which has safe data and can easly return values which works fine for API function
+// Its working fine thanks to clouser
+
+(function () {
+  const cart = [];
+  const totalPrice = 234;
+  const shippingCost = 10;
+  const totalQuantity = 22;
+
+  const addToCart = function (food, quantity) {
+    cart.push({ food, quantity });
+    console.log(`The ${food}: ${quantity} was added to the cart.`);
+  };
+
+  const orderStock = function (food, quantity) {
+    console.log(`The ${food} was added to the basket in number: ${quantity}.`);
+  };
+
+  // Here we return the object which will make public some aspect of code
+
+  return { cart, totalPrice, addToCart, totalQuantity };
+});
