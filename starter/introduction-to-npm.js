975edaa416b78887ed82537da735dcf3628880ba
Michal <m.krepiniewicz@gmail.com>
2025-06-11 19:37:14 +0200

    Very imporatant library lodash

---
+++ starter/introduction-to-npm.js
@@ -0,0 +1,12 @@
+// Step 1
+// To check if we have installed npm we can simply wirte in terminal: npm -v
+// NPm allows us to install external libraries in simple matter
+
+// Step 2
+// if NPM is installed next we have to initialize it with: npm init
+// It creates package json
+
+// Step 3 - Optional but very important library
+// There is very important library which containes many usefull methods: Lodash https://lodash.com/
+// For now where we dont use package we will install version for ES
+// npm -i (from install) lodash-es
